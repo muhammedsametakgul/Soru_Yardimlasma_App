0af780339d7d810650b293a134f4281fbe1f36e1
Muhammed Samet Akgul <msametakgul@gmail.com>
2024-04-09 15:09:16 +0300

    chore: Add a function to add question with image

--- service/createQuestions.js
+++ service/createQuestions.js
@@ -10,3 +10,30 @@ export const createQuestion = async (title, questionText) => {
     throw error; 
   }
 };
+/* 
+import { addDoc, collection } from 'firebase/firestore';
+import { ref, uploadBytes } from 'firebase/storage';
+import { FIRESTORE_DB, STORAGE } from "../config/firebaseConfig";
+
+export const uploadImageAndCreateQuestion = async (title, questionText, imageFile) => {
+  try {
+    const storageRef = ref(STORAGE, 'images/' + imageFile.name);
+    await uploadBytes(storageRef, imageFile);
+
+    const imageUrl = await getDownloadURL(storageRef);
+
+    const addedQuestion = await addDoc(collection(FIRESTORE_DB, 'question'), {
+      title: title,
+      question: questionText,
+      imageUrl: imageUrl // Firestore'a eklenen belgeye görsel URL'sini ekleyin
+    });
+
+    return addedQuestion;
+  } catch (error) {
+    console.error("Error adding document: ", error);
+    throw error; 
+  }
+};
+
+Bu fonksiyon galeriden görsel alındıktan sonra test edilecektir
+*/
