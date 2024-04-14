2e131feb66884f2c899f998860771f8129b96e36
Muhammed Samet Akgul <msametakgul@gmail.com>
2024-04-14 19:12:33 +0300

    feat: Add loading feat when data is fetching as template

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,20 +1,28 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, ScrollView, SafeAreaView, RefreshControl } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, SafeAreaView, RefreshControl, ActivityIndicator } from 'react-native';
 import ProfileBox from '../components/ProfileBox';
 import { readQuestions } from "../service/readQuestions";
 
 const HomeScreen = () => {
   const [questions, setQuestions] = useState([]);
   const [refreshing, setRefreshing] = useState(false);
+  const [loading, setLoading] = useState(true); // Yeni state ekledik: loading
 
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = async () => {
-    const fetchedQuestions = await readQuestions();
-    setQuestions(fetchedQuestions);
-    setRefreshing(false);
+    try {
+      const fetchedQuestions = await readQuestions();
+      setQuestions(fetchedQuestions);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      // Hata durumunda gerekirse kullanıcıya bir mesaj gösterebilirsiniz
+    } finally {
+      setLoading(false); // Veri yüklendikten sonra loading durumunu false olarak ayarlayın
+      setRefreshing(false);
+    }
   };
 
   const onRefresh = () => {
@@ -24,25 +32,31 @@ const HomeScreen = () => {
 
   return (
     <SafeAreaView style={styles.screen}>
-      <ScrollView
-        contentContainerStyle={styles.contentContainer}
-        refreshControl={
-          <RefreshControl
-            refreshing={refreshing}
-            onRefresh={onRefresh}
-          />
-        }
-      >
-        {questions.map((question, index) => (
-          <View key={index} style={styles.profileWrapper}>
-            <ProfileBox
-              name={question.title}
-              description={question.question}
-              imageSource={{ uri: question.imageUrl }} 
+      {loading ? ( // loading durumuna göre ActivityIndicator veya içeriği görüntüle
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+      ) : (
+        <ScrollView
+          contentContainerStyle={styles.contentContainer}
+          refreshControl={
+            <RefreshControl
+              refreshing={refreshing}
+              onRefresh={onRefresh}
             />
-          </View>
-        ))}
-      </ScrollView>
+          }
+        >
+          {questions.map((question, index) => (
+            <View key={index} style={styles.profileWrapper}>
+              <ProfileBox
+                name={question.title}
+                description={question.question}
+                imageSource={{ uri: question.imageUrl }} 
+              />
+            </View>
+          ))}
+        </ScrollView>
+      )}
     </SafeAreaView>
   );
 };
@@ -57,6 +71,11 @@ const styles = StyleSheet.create({
   profileWrapper: {
     alignItems: 'center', 
     margin: 10, 
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
