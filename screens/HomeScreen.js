c95013544d92235fc48011c9b51bf658c9db6461
Muhammed Samet Akgul <msametakgul@gmail.com>
2024-05-15 20:01:35 +0300

    feat: Add filter modal for home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -7,10 +7,15 @@ import {
   SafeAreaView,
   RefreshControl,
   ActivityIndicator,
-  Image
+  Image,
+  Modal,
+  TouchableOpacity,
+  FlatList,
 } from "react-native";
 import ProfileBox from "../components/ProfileBox";
 import { readQuestions } from "../service/readQuestions";
+import { filterQuestion } from "../service/filterQuestion";
+import { Colors } from "../utils/Colors";
 
 const motivationQuotes = [
   "Hedeflerinize bir adım daha...",
@@ -19,12 +24,27 @@ const motivationQuotes = [
   "Hayallerinizi kovalayın, başarıya doğru adım adım ilerleyin.",
   "Bugün için en iyisini yapın, yarın için en iyisini bekleyin.",
 ];
-
+const lessons = [
+  { id: 1, name: "Matematik" },
+  { id: 2, name: "Türkçe" },
+  { id: 3, name: "Tarih" },
+];
+const topics = {
+  Matematik: ["Lineer cebir", "Geometri", "Birinci Dereceden Denklemler"],
+  Türkçe: ["Ünlü Düşmesi", "Anlatım Bozuklukları", "Paragraf"],
+  Tarih: ["Osmanlı Kuruluş", "Osmanlı Yükseliş", "Osmanlı Çöküş"],
+};
 const HomeScreen = () => {
   const [questions, setQuestions] = useState([]);
   const [refreshing, setRefreshing] = useState(false);
   const [loading, setLoading] = useState(true);
   const [motivationText, setMotivationText] = useState("");
+  const [isFilterModalVisible, setIsFilterModalVisible] = useState(false);
+  const [filteredQuestions, setFilteredQuestions] = useState([]);
+  const [selectedLessonModal, setSelectedLessonModal] = useState("");
+  const [selectedTopicModal, setSelectedTopicModal] = useState("");
+  const [isLessonModalVisible, setIsLessonModalVisible] = useState(false);
+  const [isTopicModalVisible, setIsTopicModalVisible] = useState(false);
 
   useEffect(() => {
     fetchData();
@@ -35,6 +55,7 @@ const HomeScreen = () => {
     try {
       const fetchedQuestions = await readQuestions();
       setQuestions(fetchedQuestions);
+      setFilteredQuestions(fetchedQuestions);
     } catch (error) {
       console.error("Error fetching data:", error);
     } finally {
@@ -53,10 +74,62 @@ const HomeScreen = () => {
     setMotivationText(motivationQuotes[randomIndex]);
   };
 
+  const onSelectLessonAndTopic = (lesson, topic) => {
+    setSelectedLessonModal(lesson);
+    setSelectedTopicModal(topic);
+  };
+
+  const handleClearAllFilters = () => {
+    setSelectedLessonModal("");
+    setSelectedTopicModal("");
+    setFilteredQuestions(questions);
+    setIsFilterModalVisible(false);
+  };
+
+  const handleApplyFilters = async () => {
+    const filtered = await filterQuestion(
+      selectedLessonModal,
+      selectedTopicModal
+    );
+    setFilteredQuestions(filtered);
+  };
+
+  const handleLessonPress = () => {
+    setIsLessonModalVisible(true);
+  };
+
+  const handleLessonSelect = (lesson) => {
+    setSelectedLessonModal(lesson);
+    setIsLessonModalVisible(false);
+    setSelectedTopicModal("");
+    onSelectLessonAndTopic(lesson, "");
+  };
+
+  const handleTopicSelect = (topic) => {
+    setSelectedTopicModal(topic);
+    setIsTopicModalVisible(false);
+    onSelectLessonAndTopic(selectedLessonModal, topic);
+  };
+
+  const handleTopicPress = () => {
+    if (selectedLessonModal) {
+      setIsTopicModalVisible(true);
+    }
+  };
+
+  const handleClearFilters = () => {
+    setSelectedLessonModal("");
+    setSelectedTopicModal("");
+    onSelectLessonAndTopic("", "");
+  };
+
   return (
     <SafeAreaView style={styles.screen}>
       <View style={styles.header}>
-        <Image source={require('../assets/images/YKSLogoV2.png')} style={styles.logo} />
+        <Image
+          source={require("../assets/images/YKSLogoV2.png")}
+          style={styles.logo}
+        />
         <Text style={styles.headerText}>{motivationText}</Text>
       </View>
       {loading ? (
@@ -70,19 +143,104 @@ const HomeScreen = () => {
             <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
           }
         >
-          {questions.map((question, index) => (
+          {filteredQuestions.map((question, index) => (
             <View key={index} style={styles.profileWrapper}>
               <ProfileBox
                 name={question.title}
                 description={question.question}
                 imageSource={{ uri: question.imageUrl }}
                 questionId={question.id}
-                date={question.createdAt.toDate().toLocaleDateString()} 
+                date={question.createdAt.toDate().toLocaleDateString()}
               />
             </View>
           ))}
         </ScrollView>
       )}
+      <TouchableOpacity
+        style={styles.floatingButton}
+        onPress={() => setIsFilterModalVisible(true)}
+      >
+        <Text style={styles.floatingButtonText}>+</Text>
+      </TouchableOpacity>
+      <Modal
+        visible={isFilterModalVisible}
+        onRequestClose={() => setIsFilterModalVisible(false)}
+        transparent={true}
+      >
+        <View style={styles.modalOverlay}>
+          <View style={styles.modalContent}>
+            <View style={styles.listBox}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={handleLessonPress}
+              >
+                <Text style={styles.buttonText}>
+                  Ders : {selectedLessonModal || "Ders Seçiniz"}
+                </Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={styles.button}
+                onPress={handleTopicPress}
+              >
+                <Text style={styles.buttonText}>
+                  Konu : {selectedTopicModal || "Konu Seçiniz"}
+                </Text>
+              </TouchableOpacity>
+            </View>
+
+            <Modal
+              visible={isLessonModalVisible}
+              onRequestClose={() => setIsLessonModalVisible(false)}
+            >
+              <View style={styles.modalContainer}>
+                <FlatList
+                  data={lessons}
+                  renderItem={({ item }) => (
+                    <TouchableOpacity
+                      onPress={() => handleLessonSelect(item.name)}
+                    >
+                      <Text style={styles.modalItem}>{item.name}</Text>
+                    </TouchableOpacity>
+                  )}
+                  keyExtractor={(item) => item.id.toString()}
+                />
+              </View>
+            </Modal>
+
+            <Modal
+              visible={isTopicModalVisible}
+              onRequestClose={() => setIsTopicModalVisible(false)}
+            >
+              <View style={styles.modalContainer}>
+                <FlatList
+                  data={topics[selectedLessonModal]}
+                  renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => handleTopicSelect(item)}>
+                      <Text style={styles.modalItem}>{item}</Text>
+                    </TouchableOpacity>
+                  )}
+                  keyExtractor={(item) => item}
+                />
+              </View>
+            </Modal>
+
+            <TouchableOpacity
+              style={styles.clearButton}
+              onPress={handleClearFilters}
+            >
+              <Text style={styles.clearButtonText}>Filtreleri Temizle</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={styles.filterButton}
+              onPress={handleApplyFilters}
+            >
+              <Text style={styles.filterButtonText}>Filtrele</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </SafeAreaView>
   );
 };
@@ -90,28 +248,28 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    marginTop: 50
+    marginTop: 50,
   },
   header: {
-    flexDirection: 'row',
-    justifyContent: 'flex-start', 
-    backgroundColor:'#fff',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    backgroundColor: "#fff",
+    alignItems: "center",
     paddingVertical: 10,
     borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
-    marginBottom:20
+    borderBottomColor: "#ccc",
+    marginBottom: 20,
   },
   headerText: {
     fontSize: 14,
-    fontWeight: 'bold',
-    marginLeft: 10, 
-    flex: 1, 
+    fontWeight: "bold",
+    marginLeft: 10,
+    flex: 1,
   },
   logo: {
     width: 50,
     height: 50,
-    resizeMode: 'contain',
+    resizeMode: "contain",
   },
   contentContainer: {
     paddingBottom: 100,
@@ -125,6 +283,110 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  floatingButton: {
+    position: "absolute",
+    right: 20,
+    bottom: "10%",
+    backgroundColor: "#0000ff",
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  floatingButtonText: {
+    color: "#fff",
+    fontSize: 24,
+  },
+  modalOverlay: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalContent: {
+    width: 300,
+    height: 300,
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    padding: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalButtons: {
+    flexDirection: "row",
+    marginTop: 20,
+  },
+  modalButton: {
+    backgroundColor: "#0000ff",
+    padding: 10,
+    borderRadius: 5,
+    marginHorizontal: 10,
+  },
+  modalButtonText: {
+    color: "#fff",
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+  container: {
+    padding: 5,
+  },
+  button: {
+    marginBottom: 10,
+    backgroundColor: Colors.listBoxColor,
+    padding: 8,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 40,
+    backgroundColor: Colors.modalBackgroungColor,
+  },
+  modalItem: {
+    padding: 10,
+    fontSize: 18,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+    width: "100%",
+    textAlign: "center",
+  },
+  listBox: {
+    backgroundColor: Colors.modalBackgroungColor,
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+  clearButton: {
+    backgroundColor: "#ff0000",
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+    alignItems: "center",
+  },
+  clearButtonText: {
+    color: "#fff",
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+  filterButton: {
+    backgroundColor: "#00FF00",
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+    alignItems: "center",
+  },
+  filterButtonText: {
+    color: "#FFFFFF",
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+  
 });
 
 export default HomeScreen;
